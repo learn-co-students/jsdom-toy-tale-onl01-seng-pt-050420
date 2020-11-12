72b735064172c2fd9173b80037cad87b836d2ed3
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-12 02:49:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,50 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function submitData(toyName, toyImg, toyLikes)
+  {
+    const formData = {
+      name: toyName,
+      img: toyImg,
+      likes: toyLikes
+    }
+
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    }
+
+    return fetch("http://localhost:3000/toys", configObj)
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(function(object) {
+        for (let key in object)
+        {
+          let div = document.createElement("div").setAttribute("class", "card");
+          let h2 = document.createElement("h2");
+          let img = document.createElement("img");
+          let p = document.createElement("p");
+          let btn = document.createElement("button");
+          Object.assign(img, {
+            className: "toy-avatar",
+            src: object["image"]
+          })
+          btn.setAttribute("class", "lik-btn");
+          h2.innerHTML = object["name"];
+          p.innerHTML = object["likes"];
+          div.appendChild(h2);
+          div.appendChild(img);
+          div.appendChild(p);
+          div.appendChild(btn);
+          document.body.appendChild(div);
+        } 
+      })
+  }
+
 });
